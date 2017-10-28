a2348fe0a6f810fb1f0bff36c0a536fe269d53d6
cd <cdelamarre@gmail.com>
2017-10-29 00:26:25 +0200

    revision du JS
    Ajout de fillD2D_PO_EnteteSyntheseContainer
    Ajout de fillD2D_PO_TracaAmontContainer

---
+++ script/D2D_PO.js
@@ -0,0 +1,33 @@
+fillContainers = function (){
+var whichNumPo = $('#whichNumPo').val();
+        alert(whichNumPo);
+        fillD2D_PO_EnteteSyntheseContainer (whichNumPo);
+        fillD2D_PO_TracaAmontContainer (whichNumPo);
+        }
+
+var baseUrlToLoad = 'http://yrocher.fcsystem.com/Symfony/WsGetJsonFromSql.php?apikey=595c015a-98c4-4097-9d89-c5b83ed28ff1&isonlyresult=TRUE';
+        fillD2D_PO_EnteteSyntheseContainer = function  (whichNumPo){
+        var urlToLoad += baseUrlToLoad + '&sqlrfilepath=D2D_PO_EnteteSynthese.sql&num_po' + whichNumPo;
+                $.ajax({
+                url:urlToLoad,
+                        dataType: 'json',
+                        success: function (json) {
+                        example2 = $('#D2D_PO_EnteteSyntheseContainer').columns({
+                        data: json,
+                        });
+                        }
+                });
+        }
+
+fillD2D_PO_TracaAmontContainer = function  (whichNumPo){
+var urlToLoad += baseUrlToLoad + '&sqlrfilepath=D2D_PO_TracaAmont.sql&num_po' + whichNumPo;
+        $.ajax({
+        url:urlToLoad,
+                dataType: 'json',
+                success: function (json) {
+                example2 = $('#D2D_PO_TracaAmontContainer').columns({
+                data: json,
+                });
+                }
+        });
+        }
