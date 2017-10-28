66ea8fb972d94640bbae9d7a5fffc0680b2472fb
cd <cdelamarre@gmail.com>
2017-10-28 23:13:22 +0200

    Divers 20171027
    Externalisation des functions
    Ajout de remplacement dynamique des variables num_po et sku dans les requetes sqlr

--- WsGetJsonFromSql.php
+++ WsGetJsonFromSql.php
@@ -1,15 +1,25 @@
 <?php
 
+header("Access-Control-Allow-Origin: http://com.e-solutions.tm.fr");
+header('content-type:application/json;charset=SQL_ASCII');
+
+require 'WsGetJsonFromSqlLib.php';
+?>
+<?php
+
 //POUR TESTER
 //http://test-cotyr.fcsystem.com/ws/WsGetQfPolWithTarif.ws_json?apikey=595c015a-98c4-4097-9d89-c5b83ed28ff1
 
 $dbug = 0;
-$title = "Liste des ports de dÃ©part avec tarifs";
+$title = "Recup de JSON à partir d'un fichier SQL";
 $release = "1.0.1";
 $release_date = "20170605";
 $ws = preg_replace('/(\w+)\.\w+$/i', '$1', basename(__FILE__));
 //https://phea.fr/outils/generateur-cle-aleatoire-random-key
 $apiKey = "595c015a-98c4-4097-9d89-c5b83ed28ff1";
+
+
+$dbconn = getDbConn();
 
 $getApiKey = $_GET["apikey"];
 $getPortCode = $_GET["portcode"];
@@ -21,32 +31,12 @@ $getColumns = $_GET["columns"];
 $getExtension = $_GET["ext"];
 $getIsOnlyResult = $_GET["isonlyresult"];
 $sSqlrFilePath = $_GET["sqlrfilepath"];
+$num_po = $_GET["num_po"];
+$sku = $_GET["sku"];
 
 $sqlr = file_get_contents($sSqlrFilePath, FILE_USE_INCLUDE_PATH);
-
-header('content-type:application/json;charset=SQL_ASCII');
-
-define('PGHOST', "localhost");
-define('PGPORT', "5432");
-define('PGDATABASE', "yrocher");
-define('PGUSER', "postgres");
-define('PGPASSWORD', "postgres");
-define('PGCLIENTENCODING', 'UNICODE');
-define('ERROR_ON_CONNECT_FAILED', 'Sorry, can not connect the database server now!');
-
-$dbconn = pg_connect("host=" . PGHOST . " port=" . PGPORT . " dbname=" . PGDATABASE . " user=" . PGUSER . " password=" . PGPASSWORD)
-        or die('Connexion impossible : ' . pg_last_error());
-
-function convert_from_latin1_to_utf8_recursively($dat) {
-    if (is_string($dat))
-        return utf8_encode($dat);
-    if (!is_array($dat))
-        return $dat;
-    $ret = array();
-    foreach ($dat as $i => $d)
-        $ret[$i] = self::array_utf8_encode($d);
-    return $ret;
-}
+$aVars = getSqlrVars();
+$sqlr = replace_num_po_in_sql($aVars, $sqlr);
 
 $result = pg_query($sqlr) or die('Echec de la requete : ' . pg_last_error());
 $resultRows = pg_num_rows($result);
@@ -84,7 +74,7 @@ $arrayMain = array(
             "portname=[a-Z]",
             "ispol=[TRUE|FALSE]",
             "ispod=[TRUE|FALSE]",
-            "ext=[extension]( pour une gÃ©nÃ©ration  )",
+            "ext=[extension]( pour une génération  )",
             "isonlyresult=[TRUE|FALSE]"
         )
     ),
@@ -124,6 +114,7 @@ if ($getExtension != '') {
     fputs($fp, $strJson); // write the data in the opened file
     fclose($fp); // close the file	
 } else {
-    echo $strJson;
+    print $strJson;
 }
+
 ?>	
---
+++ ePoTracaAmont.sql
@@ -0,0 +1,18 @@
+SELECT
+ph.num_po, 
+pd.set_article_acheteur as "Sku#", 
+pt.date_min as "Date départ",
+'' as CQ, 
+pt.mer_nom_port_embarquement||' '||pt.mer_nom_port_embarquement as "lieu départ", 
+ph.mode_transport as "type de transport", 
+pt.mer_port_arrivee||' '||pt.mer_nom_port_arrivee as "lieu arrivée", 
+pt.date_arrivee_entrepot as "Date arrivée ETA ou ATA ou Arrival POD", 
+'' as "qté embarquée"
+
+
+FROM po_header as ph
+LEFT JOIN po_transport as pt
+ON ph.num_po = pt.num_po
+LEFT JOIN po_detail as pd
+ON pt.num_po = pd.num_po
+WHERE LOWER(SPLIT_PART(ph.num_po, '_', '1')) = LOWER('[[num_po]]')
